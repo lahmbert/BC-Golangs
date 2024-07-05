ece79e765a26f933d90b1c907a0950ae081f3a82
Muhammad Sabran <muhammadsabran86.com>
2024-07-05 23:25:51 +0800

    Nomor 3

--- Weekly_Test01/Quiz03/main.go
+++ Weekly_Test01/Quiz03/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	word := []string{"this", "is", "a", "kanoha"}
+	except := []string{"is", "a"}
+	result := capitalize(word, except)
+	fmt.Println(result)
+}
+
+func capitalize(words []string, except []string) []string {
+	for i, word := range words {
+		target := false
+		for _, ex := range except {
+			if word == ex {
+				target = true
+				break
+			}
+		}
+		if !target {
+			words[i] = strings.Title(word)
+		}
+	}
+	return words
 }
---
+++ Weekly_Test01/Quiz04/go.mod
@@ -0,0 +1,3 @@
+module Quiz04
+
+go 1.22.4
