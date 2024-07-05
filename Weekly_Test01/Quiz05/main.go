7a7ea4ea247aca229a007f21d94379c56e070eb8
Muhammad Sabran <muhammadsabran86.com>
2024-07-05 23:48:45 +0800

    Nomor 5

--- Weekly_Test01/Quiz05/main.go
+++ Weekly_Test01/Quiz05/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func main() {
+	nums := []int{1, 2, 3, 10, 4, 6, 5, 8, 9, 7}
+	sort.Ints(nums)
+	result := oddBeforeEven(nums)
+	fmt.Println(result)
+}
+
+func oddBeforeEven(nums []int) []int {
+	even := []int{}
+	odd := []int{}
+
+	for _, v := range nums {
+		if v%2 != 0 {
+			even = append(even, v)
+		} else {
+			odd = append(odd, v)
+		}
+	}
+
+	return append(even, odd...)
 }
