6fdb5e01e3f2a9684bba0a72c4bfe007b3da33c4
Muhammad Sabran <muhammadsabran86.com>
2024-07-06 00:09:57 +0800

    Nomor 6

--- Weekly_Test01/Quiz06/main.go
+++ Weekly_Test01/Quiz06/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// nums := []int{1, 2, 3, 4}
+	// nums := []int{1, 4, 8, 9}
+	nums := []int{9, 9, 9, 9}
+	result := plusOne(nums)
+	fmt.Println(result)
+}
+
+func plusOne(nums []int) []int {
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] < 9 {
+			nums[i]++
+			return nums
+		} else {
+			nums[i] = 0
+		}
+	}
+	return append([]int{1}, nums...)
 }
