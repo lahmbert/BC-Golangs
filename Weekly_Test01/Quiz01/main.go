207127813c16dd5676a6d485409e051b74b7abe6
Muhammad Sabran <muhammadsabran86.com>
2024-07-05 21:20:08 +0800

    Nomor 1 Selesai

--- Weekly_Test01/Quiz01/main.go
+++ Weekly_Test01/Quiz01/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	words := "bananas"
+	result := removeDuplicateLetter(words)
+	fmt.Printf("%s\n", result)
+}
+
+func removeDuplicateLetter(words string) string {
+	var result string
+	var sameStr string
+	for _, value := range words {
+		same := false
+		for i := 0; i < len(sameStr); i++ {
+			if rune(sameStr[i]) == value {
+				same = true
+				break
+			}
+		}
+		if !same {
+			sameStr += string(value)
+			result += string(value)
+		}
+	}
+
+	return result
 }
