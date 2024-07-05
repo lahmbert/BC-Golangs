81847c8e887f8f0586a97aabfcfed4103af55d4b
Muhammad Sabran <muhammadsabran86.com>
2024-07-06 01:27:05 +0800

    Nomor 10

--- Weekly_Test01/Quiz09/main.go
+++ Weekly_Test01/Quiz09/main.go
@@ -9,7 +9,6 @@ func main() {
 	nums := []int{1, 1, 1, 2, 2, 2, 3, 3, 3}
 	result := countFrequentElement(nums)
 
-	// Mencetak hasil dalam format "elemen=jumlah"
 	for key, value := range result {
 		fmt.Printf("%d=%d, ", key, value)
 	}
@@ -19,7 +18,6 @@ func main() {
 func countFrequentElement(nums []int) map[int]int {
 	frequency := make(map[int]int)
 
-	// Menghitung frekuensi setiap elemen dalam nums
 	for _, num := range nums {
 		frequency[num]++
 	}
--- Weekly_Test01/Quiz10/main.go
+++ Weekly_Test01/Quiz10/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	nums1 := []int{1, 2, 3}
+	nums2 := []int{4, 5, 6}
+	// nums1 := []int{9, 2, 7}
+	// nums2 := []int{1, 3, 5}
+
+	result := addDigits(nums1, nums2)
+
+	fmt.Println(result)
+}
+
+func addDigits(nums1 []int, nums2 []int) []int {
+	resultLen := len(nums2) + 1
+	result := make([]int, resultLen)
+
+	i, j := len(nums1)-1, len(nums2)-1
+	k := resultLen - 1
+
+	for i >= 0 || j >= 0 {
+		sum := result[k]
+
+		if i >= 0 {
+			sum += nums1[i]
+		}
+		if j >= 0 {
+			sum += nums2[j]
+		}
+
+		result[k] = sum % 10
+		if sum >= 10 {
+			result[k-1] += sum / 10
+		}
+
+		i--
+		j--
+		k--
+	}
+
+	if result[0] == 0 {
+		result = result[1:]
+	}
+
+	return result
 }
